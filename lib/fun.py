8d4c5dc0a4673c8248df3cfcf43c2b89202a7dea
Primrose <primrose.njoki@student.moringaschool.com>
2025-05-08 15:10:57 +0300

    loops

---
+++ lib/fun.py
@@ -0,0 +1,14 @@
+# i=0
+# while i<5:
+#     print("oops!")
+
+# print(7)
+# for i in range(10):
+#     print ("lol")
+#     print(f"i is:{i}")
+# 
+player_heights = [0.008, 0.008, 0.008, 0.009, 0.008, 0.01, 0.009, 0.009, 0.01, 0.008, 0.009, 0.009, 0.008, 0.008, 0.008, 0.009, 0.008, 0.009, 0.01, 0.01]
+for height in player_heights:
+    player_heights_height = height * 7920
+    
+print(player_heights)
--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    counter=10
+    while counter >0:
+        print (counter)
+        counter-=1
+    print("Happy New Year!")
+happy_new_year()
+
     # code goes here!
-    pass
+    #pass
 
 def square_integers(int_list):
+    return [num ** 2 for num in int_list]
+print(square_integers([1,2,3,4,5]))
+# def square_integers(int_list):
+#     return [num ** 2 for num in int_list]
+    
+# print(square_integers([1,2,3,4,5]))  # Should output [1, 4, 9, 16, 25]
+
+    
     # code goes here!
-    pass
+    #pass
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1,101):
+        if num % 3==0 and num % 5==0:
+            print('FizzBuzz')
+        elif num % 3==0:
+            print("Fizz")
+        elif num % 5==0:
+            print("Buzz")
+        else :
+            print(num)
+
+fizzbuzz()
+        
+    # code goes here! pass
